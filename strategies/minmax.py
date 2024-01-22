4c1a36b8c3615eb3235c74fdd3064f3ce25432d2
Davide Natale <s318967@studenti.polito.it>
2024-01-23 00:04:30 +0100

    minmax fixed

--- strategies/minmax.py
+++ strategies/minmax.py
@@ -1,62 +1,100 @@
-from game import Game,Move
+from game import Game
 from copy import deepcopy
 import sys
 import numpy as np
-def wrap_min_max(board, player_id):
-    return minmax(board,player_id)[0]
-def minmax(board, player_id, depth=4, alpha=-sys.maxsize, beta=sys.maxsize):
-    val = board.check_winner()
-    best_ply = None
-    possible = board.getPossibleMoves(player_id)
 
-    if val != -1 or not possible or depth == 0:
-        if val == 0:
-            val = float('inf')
-        elif val == 1:
-            val = float('-inf')
-        return best_ply, val
+def calculate_occurences(board, player_id):
+    occCount = 0
+    for i in range(5):
+        row = board[i, :]
+        col = board[:, i]
+        x = np.where(row == player_id)[0]
+        y = np.where(col == player_id)[0]
 
-    #evaluations = list()
-    prev_player_id = player_id
+        if len(x) == 4:
+            ok = (x[-1] - x[0] == len(x) - 1)
+            if ok:
+                occCount += 1
 
-    if player_id == 1:
-        val = float('inf')
-        player_id = 0
+        if len(y) == 4:
+            ok = y[-1] - y[0] == len(y) - 1
+            if ok:
+                occCount += 1
+    return occCount
+
+def fitness(game, player_id, depth):
+    winner = game.check_winner()
+
+    if winner == 0:     ## Maximizer won (X)
+        return 100 + depth
+    elif winner == 1:   ## Minimizer won (O)
+        return -100 - depth
     else:
-        val = float('-inf')
-        player_id = 1
+        value = 0
+        board = game.get_board()
+        occValue = calculate_occurences(board, player_id) * 5
 
-    for ply in possible:
-        tmp = deepcopy(board)
-        acc = tmp.move(ply[0], ply[1], prev_player_id)
+        if board[2, 2] == 0:
+            value += 20
+        elif board[2, 2] == 1:
+            value -= 20
 
-        if acc == False:
-            print(acc, player_id, "con mosse :", ply, "\n", board.get_board())
-        
-        _, tmp1 = minmax(tmp, player_id, depth - 1, -beta, -alpha)
-        #evaluations.append((ply, -val))
-        if tmp1 > val:
-            val = tmp1
-            best_ply = ply
+        if player_id == 1:
+            occValue *= -1
 
-        alpha = max(alpha, -val)
-        
-        if alpha >= beta:
-            break  # Taglio beta
-   
-    #print(best_ply, val)
-    return best_ply, -val#max(evaluations, key=lambda k: k[1])
+        frequency = np.unique(board, return_counts=True)
+        uniqueCount = len(frequency[0])
+        if uniqueCount == 3:      ## Contains blank pieces
+            maximizerPieceCount = frequency[1][1]
+            minimizerPieceCount = frequency[1][2]
+        elif uniqueCount == 2:
+            maximizerPieceCount = frequency[1][0]
+            minimizerPieceCount = frequency[1][1]
 
-if __name__ == '__main__':
-    from strategies.utils import CustomGame
-    from strategies.utils import MinMaxPlayer,RandomPlayer
-    cnt=0
-    for i in range(100):
-        game=CustomGame()
-        player1=MinMaxPlayer()
-        player2=RandomPlayer()
-        winner=game.play(player1,player2)
-        if winner==1:
-            cnt+=1
+        value += (maximizerPieceCount - minimizerPieceCount) + occValue
+        return value
+
+def wrap_min_max(game: Game):
+    return minmax(game, game.get_current_player())[0]
+
+def minmax(game, player_id, alpha=-sys.maxsize, beta=sys.maxsize, depth=2):
+    winner = game.check_winner()
     
-    print(cnt)
+    if winner != -1 or depth == 0:
+        score = fitness(game, player_id, depth)
+        return None, score
+
+    best_ply = None
+    possible_moves = game.getPossibleMoves(player_id) 
+    
+    if player_id == 0:      ## Player X 
+        new_player_id = 1
+
+        for ply in possible_moves:
+            tmp = deepcopy(game)
+            tmp.move(ply[0], ply[1], player_id)
+            _, val = minmax(tmp, new_player_id, alpha, beta, depth - 1)
+
+            if val > alpha:
+                alpha = val
+                best_ply = ply
+            
+            if alpha >= beta:
+                break
+        return best_ply, alpha
+    
+    else:       ## Player O
+        new_player_id = 0
+
+        for ply in possible_moves:
+            tmp = deepcopy(game)
+            tmp.move(ply[0], ply[1], player_id)
+            _, val = minmax(tmp, new_player_id, alpha, beta, depth - 1)
+
+            if val < beta:
+                beta = val
+                best_ply = ply
+            
+            if alpha >= beta:
+                break
+        return best_ply, beta
